d3908d00f18ee4052b47803e409d82856c4c4092
Madone <ahamad.jedu@gmail.com>
2016-08-05 15:44:01 +0700

    Create base controller

---
+++ controllers/BasicConcontroller.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ */
+
+namespace anda\core;
+
+/**
+ * Description of BasicConcontroller
+ *
+ * @author Andasoft
+ */
+class BasicConcontroller extends \yii\web\Controller{
+    //put your code here
+    
+    
+    
+}
