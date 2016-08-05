423e29d3189f32f3930677540dd2b5ff930fea50
Madone <ahamad.jedu@gmail.com>
2016-08-05 15:13:01 +0700

    create method

--- Test.php
+++ Test.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace anda\core;
-
-
 
 class Test {
 
-public function One(){
-return 'one';
-}
+    public function One() {
+        return 'one';
+    }
+
+    public static function Two() {
+        return 'Two';
+    }
 
 }
